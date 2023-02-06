32d1485a456678506b6d71a13ad08506b1f60c12
Mathieu Segaud <segaud.mathieu@gmail.com>
2023-02-06 15:50:45 +0100

    add d'une icone pour masquer les projets en mobile

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { projects } from "../data/projects";
 import Modal from "./Modal";
 
 const MyProjects = () => {
-	const [projectId, setProjectId] = useState('');
-
+	const [projectId, setProjectId] = useState("");
+	
+	const toggle = () => {
+		const projectContainer = document.querySelector(".projects-container");
+		projectContainer.classList.toggle("hide");
+	};
 	return (
 		<section id="myProjects">
-			<h3>Mes projets :</h3>
+			<h3>
+				<i className="fa-solid fa-hashtag" onClick={toggle}></i>
+				Mes projets :
+			</h3>
+
 			<div className="projects-container">
 				{projects.map(({ id, title, text, cover, site, code }) => (
 					<ProjectCard
