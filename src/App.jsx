97f99835626af1cc08dfd2bd124337669d88ef6a
Mathieu Segaud <segaud.mathieu@gmail.com>
2023-02-17 17:35:54 +0100

    suppression des fichers scss du test de projets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddProject from "./AddProject";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./Home";
